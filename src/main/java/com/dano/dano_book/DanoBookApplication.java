ec543520daf4781e50cfba2811c80800f45abc96
dano <ayoub18daoui@gmail.com>
2025-01-16 11:29:50 +0100

    first commit

---
+++ src/main/java/com/dano/dano_book/DanoBookApplication.java
@@ -0,0 +1,13 @@
+package com.dano.dano_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanoBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanoBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dano/dano_book/DanoBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dano.dano_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanoBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
